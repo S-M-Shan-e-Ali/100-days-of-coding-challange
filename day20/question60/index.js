3fd77d6b60bc6c43a9fdaabef9acc9a9c5310ec1
Syed Muhammad Shan e Ali <143424766+S-M-Shan-e-Ali@users.noreply.github.com>
2024-03-21 22:00:45 +0500

    Add files via upload

---
+++ day20/question60/index.js
@@ -0,0 +1,18 @@
+/* Question : 60
+        Self-Running User Profile: Create a quick, self-setup profile for a user that can tell you the user's name and age.
+
+        Explain & TIP : This is like a self-building lego set. Once you start it, it builds a user profile by itself and can tell you about the user.
+*/
+// This profile sets itself up and can share info about the user
+var userProfile = (function () {
+    // The user's details are kept inside
+    var name = "John";
+    var age = 30;
+    //This part shares the user's details
+    return { displayInfo: function () {
+            console.log("Name: ".concat(name, ", Age: ").concat(age));
+        } };
+})();
+//Asking he profile to tell us about the user
+userProfile.displayInfo(); // It says the user's name and age
+// We made a self-setup profile that can talk about the user.
