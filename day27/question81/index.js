553db1198eebafb009edc523d492e41592452f9a
Syed Muhammad Shan e Ali <143424766+S-M-Shan-e-Ali@users.noreply.github.com>
2024-03-30 23:42:43 +0500

    Add files via upload

---
+++ day27/question81/index.js
@@ -0,0 +1,14 @@
+/* Question : 81
+        Iterating Over Object Properties: Write a function that takes an object as an argument and logs all of its properties and values.
+
+        Explain & TIP: You can loop through each property of an object using a 'for ... in loop'. This is useful for when you need to examine or display all the information object holds.
+*/
+// This function shows every detail about an object
+function logObjectProperties(obj) {
+    for (var property in obj) { // Loops through each property in the object
+        console.log("".concat(property, ":").concat(obj[property])); // Shows the property name and its value
+    }
+}
+// Using the function with a car object 
+logObjectProperties({ make: "Toyota", model: "Corolla", year: "2021", color: "blue" });
+// It tells us each piece of information stored about the car.
