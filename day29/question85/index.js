f52c0b70c6a3e85bcd79571936b7eea912101d6c
Syed Muhammad Shan e Ali <143424766+S-M-Shan-e-Ali@users.noreply.github.com>
2024-03-31 01:06:24 +0500

    Add files via upload

---
+++ day29/question85/index.js
@@ -0,0 +1,12 @@
+/*  Question : 85
+        Finding the Position of a Substring: Write a function that locates the first occurrence of the word "code" withing any given string and returns its position.
+
+        Explain & TIP : Use  .indexOf() to find where a certain piece of text starts within a larger string. If the text isn't found, this method return -1.
+*/
+// This function finds where "code" first shows up in a text 
+function findCodePosition(str) {
+    return str.indexOf("code"); // Looks for "code" and tells where it found it
+}
+// Example : Searching within a sentence
+console.log(findCodePosition("Learn to code with JavaScript")); // Outputs the start position of "code"
+// It tells us the position number where "code" starts.
