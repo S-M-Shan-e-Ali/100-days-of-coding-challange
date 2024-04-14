e625de25b74bf6f56b9c8e0692a0845e3377d93e
Syed Muhammad Shan e Ali <143424766+S-M-Shan-e-Ali@users.noreply.github.com>
2024-04-14 18:35:56 +0500

    Add files via upload

---
+++ day34/question102/index.js
@@ -0,0 +1,9 @@
+/* Question : 102
+        Calculate and log the absolute value of a number, which is useful for finding the magnitude of a difference without regard to direction
+
+        Explain & TIP: The Math.abs() function returns the absolute value of a number, which is useful for finding the magnitude of a difference without regard to direction.
+*/
+//  Calculate the absolute difference between -5 and 5 
+var difference = Math.abs(-5 - 5);
+console.log(difference); //  Outputs : 10
+// This line shows the difference between -5 and -5, ignoring the sign.
---
+++ day34/question102/index.ts
@@ -0,0 +1,13 @@
+
+/* Question : 102
+        Calculate and log the absolute value of a number, which is useful for finding the magnitude of a difference without regard to direction
+
+        Explain & TIP: The Math.abs() function returns the absolute value of a number, which is useful for finding the magnitude of a difference without regard to direction.
+*/
+
+//  Calculate the absolute difference between -5 and 5 
+
+const difference : number = Math.abs (-5 -5);
+
+console.log(difference);  //  Outputs : 10
+// This line shows the difference between -5 and -5, ignoring the sign.
