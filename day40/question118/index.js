69d0d9e5c274fea76621715c9f13137032228d8b
Syed Muhammad Shan e Ali <143424766+S-M-Shan-e-Ali@users.noreply.github.com>
2024-04-14 23:11:17 +0500

    Add files via upload

---
+++ day40/question118/index.js
@@ -0,0 +1,10 @@
+/* Question : 118
+        Write a loop that logs numbers from 1 to 10 to the console.
+
+        Explain & TIP : A for loop is a concise way to run a block of code a specific number of times. It's perfect for when you know exactly how many iterations you need.
+*/
+// This loop logs numbers from 1 to 10
+for (var i = 1; i <= 10; i++) {
+    console.log(i); // Logs the current value of i
+}
+// Each iteration increases the value of i by 1, logging it until it reaches 10.
