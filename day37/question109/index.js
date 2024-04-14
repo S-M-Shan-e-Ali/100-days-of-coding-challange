5469e7127272dc08918a1e681319c4fde4db9464
Syed Muhammad Shan e Ali <143424766+S-M-Shan-e-Ali@users.noreply.github.com>
2024-04-14 20:33:56 +0500

    Add files via upload

---
+++ day37/question109/index.js
@@ -0,0 +1,13 @@
+/*  Question 109:
+        Write an if statement that logs "Good Morning" if the current time is before 12 PM.
+
+        Explain & TIP : You can get the current hour using the Date object and its getHours() method. Remember, hours are in 24-hour format, so 12 PM is 12.
+*/
+// Checks the current hour and logs "Good Morning" if it's before 12 PM
+var currentTime = new Date();
+if (currentTime.getHours() < 12) {
+    console.log("Good Morning"); // It's morning if before 12 PM
+}
+else
+    console.log("Good After Noon"); // It's After Noon if after 12 PM}
+// This simple check helps us greet users appropriately based on the time of day.
---
+++ day37/question110/index.ts
@@ -0,0 +1,17 @@
+
+/* Question : 110
+        Create a function that assigns a grade (A, B, C, D, F) based on a student's score.
+
+        Explain & TIP : A common grading system assigns letter grades based on score ranges. You can use an if-else-if chain to determine the grade.
+*/
+
+function assignGrade(score: number): string {
+       if (score >= 90) {  return "A"; 
+} else if (score >= 80) {  return "B"; 
+} else if (score >= 70) {  return "C";
+} else if (score >= 60) {  return "D";
+} else { return "F"; }
+}
+  console.log(assignGrade(85)); // Outputs: B
+  console.log(assignGrade(55)); // Outputs: F
+  
