5912cdde385b38e2074f40eb0bf8e9ba533ad6ca
Syed Muhammad Shan e Ali <143424766+S-M-Shan-e-Ali@users.noreply.github.com>
2024-05-14 00:29:09 +0500

    Add files via upload

---
+++ projects/cli Word Counter/index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import chalkAnimation from "chalk-animation";
+import { start } from "./start.js";
+console.clear();
+
+const sleep=()=>{
+    return new Promise((res)=>{setTimeout(res, 2000)})
+    }
+
+async function welcome() {        
+    let rainbowTitle=chalkAnimation.rainbow('\t\t\tLet Start The Word Counter.\n\n');
+    await sleep();
+    rainbowTitle.stop();
+}
+    
+await welcome();
+
+start();
---
+++ projects/cli Word Counter/start.js
@@ -0,0 +1,44 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+export async function start() {
+    let shouldExit = false;
+    do {
+        let ask = await inquirer.prompt({
+            type: "list",
+            name: "rep",
+            message: "\nSelect Any One",
+            choices: ["Character Count", "Word Count", "Character & Word Count", "Exit"]
+        });
+        if (ask.rep === "Exit") {
+            shouldExit = true;
+        }
+        else {
+            let paragraph = await inquirer.prompt({
+                type: "input",
+                name: "para",
+                message: chalk.red("\nWrite A Paragraph.\n"),
+                validate: (input) => input.trim().length > 0 || "Please enter a paragraph",
+            });
+            let text = paragraph.para;
+            // Remove whitespace from the text
+            const textWithoutWhitespace = text.replace(/\s/g, '');
+            // Count characters (including letters, digits, symbols)
+            const characterCount = textWithoutWhitespace.length;
+            // Count words (assuming words are separated by whitespace)
+            const words = text.split(/\s+/);
+            const wordCount = words.length;
+            switch (ask.rep) {
+                case "Character Count":
+                    console.log("\nCharacter count:", characterCount);
+                    break;
+                case "Word Count":
+                    console.log("\nWord count :", wordCount);
+                    break;
+                case "Character & Word Count":
+                    console.log("\nCharacter count:", characterCount);
+                    console.log("Word count :", wordCount);
+                    break;
+            }
+        }
+    } while (!shouldExit);
+}
